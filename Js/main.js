b57c493225f39e37a6e52f0037e653a9930a4950
Kishanofficial8899 <kishanpatel3545@gmail.com>
2019-12-19 10:46:01 +0530

    Final Commit

---
+++ Js/main.js
@@ -0,0 +1,44 @@
+const SelectElement = (element) => {
+  return document.querySelector(element);
+};
+let MenuToggle = SelectElement('.menu-toggle');
+let body = SelectElement('body');
+let bars = SelectElement('.fa-bars');
+MenuToggle.addEventListener('click', function() {
+  body.classList.toggle('open');
+});
+
+// fa-timestme
+
+window.sr = ScrollReveal();
+
+//Scroll Revel
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
+});
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+sr.reveal('.animate-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 600
+});
+
+//FOR THE LIGHTBOX
+$(function() {
+  const $gallery = $('.gallery img').simpleLightbox();
+});
